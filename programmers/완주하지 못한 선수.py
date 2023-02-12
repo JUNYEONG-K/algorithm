4e4cf1f23fc1b143dd7b40b70f661e2781616ff2
JUNYEONG-K <godoly1211@naver.com>
2023-02-12 22:04:47 +0900

    백준 문제풀이 0212

--- programmers/완주하지 못한 선수.py
+++ programmers/완주하지 못한 선수.py
@@ -10,7 +10,29 @@ def solution(participant, completion):
             return participant[i]
     return participant[-1]
 
+
 # Counter 객체 활용
 def solution2(participant, completion):
     answer = Counter(participant) - Counter(completion)
     return list(answer.keys())[0]
+
+
+def solution3(participant, completion):
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        print(dic)
+        temp += int(hash(part))
+        print(temp)
+    for com in completion:
+        temp -= hash(com)
+        print(temp)
+    answer = dic[temp]
+
+    return answer
+
+
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+solution3(participant, completion)
